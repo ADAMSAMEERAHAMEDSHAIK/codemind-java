3384fedff8b2309b4e781f827bdfb79068fc8007
ADAMSAMEERAHAMEDSHAIK <142864954+ADAMSAMEERAHAMEDSHAIK@users.noreply.github.com>
2024-01-20 17:27:14 +0530

    import java.util.Scanner; public class CodeMind {     public static void main(String args[])     {         Scanner sc = new Scanner(System.in);         String n = sc.next();         int d = Integer.parseInt(n,8);         System.out.println(d);
        }
    }

---
+++ Octal_to_Decimal_Conversion.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class CodeMind
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        String n = sc.next();
+        int d = Integer.parseInt(n,8);
+        System.out.println(d);
+        
+    }
+}
+
