feabb3b6a6e31c9a73fbc0ca188d4ead22b1a88c
ADAMSAMEERAHAMEDSHAIK <142864954+ADAMSAMEERAHAMEDSHAIK@users.noreply.github.com>
2024-01-20 17:21:35 +0530

    import java.util.Scanner; public class CodeMind {     public static void main(String[] args)     {         int n,t,r=0,c,d;         Scanner sc = new Scanner(System.in);         n = sc.nextInt();         while(true)         {             n++;             t=n;             while(t>0)             {                 r = r*10;                 r = r + t%10;                 t = t/10;             }             if(r==n)             {                 d = (int) Math.sqrt(n);                 for(c=2; c<=d; c++)                 {                     if(n%c==0)                         break;                 }                 if(c==d+1)                     break;             }             r=0;         }         System.out.println(n);     } }

---
+++ Program_to_find_out_the_next_prime_palindrome_number.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class CodeMind
+{
+    public static void main(String[] args) 
+    {
+        int n,t,r=0,c,d;
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        while(true) 
+        {
+            n++;
+            t=n;
+            while(t>0) 
+            {
+                r = r*10;
+                r = r + t%10;
+                t = t/10;
+            }
+            if(r==n) 
+            {
+                d = (int) Math.sqrt(n);
+                for(c=2; c<=d; c++) 
+                {
+                    if(n%c==0)
+                        break;
+                }
+                if(c==d+1)
+                    break;
+            }
+            r=0;
+        }
+        System.out.println(n);
+    }
+}
