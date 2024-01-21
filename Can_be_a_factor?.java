620e9f35eb7df923415707f380e3d0dcec9c693b
ADAMSAMEERAHAMEDSHAIK <142864954+ADAMSAMEERAHAMEDSHAIK@users.noreply.github.com>
2024-01-21 10:22:52 +0530

    import java.util.Scanner; public class CodeMind {     public static void main(String args[])     {         Scanner sc = new Scanner(System.in);         int t = sc.nextInt();         while(t-- > 0)         {             int a,b,z;             a = sc.nextInt();             b = sc.nextInt();             z = Math.abs(a-b);             if(z>=a || a==b)             {                 System.out.println("YES");             }             else             {                 System.out.println("NO");             }         }     } }

---
+++ Can_be_a_factor?.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class CodeMind
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {
+            int a,b,z;
+            a = sc.nextInt();
+            b = sc.nextInt();
+            z = Math.abs(a-b);
+            if(z>=a || a==b)
+            {
+                System.out.println("YES");
+            }
+            else
+            {
+                System.out.println("NO");
+            }
+        }
+    }
+}
