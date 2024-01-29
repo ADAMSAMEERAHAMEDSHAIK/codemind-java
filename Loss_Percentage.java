b9d57bd4fb9f7d83a0cb3326a8cffa15ff5cc80d
ADAMSAMEERAHAMEDSHAIK <142864954+ADAMSAMEERAHAMEDSHAIK@users.noreply.github.com>
2024-01-29 21:12:26 +0530

    import java.util.Scanner; public class CodeMind {     public static void main(String[] args)     {         Scanner sc = new Scanner(System.in);         float cp = sc.nextFloat();         float sp = sc.nextFloat();         float c = ((cp-sp)/cp)*100;         System.out.printf("%.2f",c);     } }

---
+++ Loss_Percentage.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class CodeMind
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        float cp = sc.nextFloat();
+        float sp = sc.nextFloat();
+        float c = ((cp-sp)/cp)*100;
+        System.out.printf("%.2f",c);
+    }
+}
