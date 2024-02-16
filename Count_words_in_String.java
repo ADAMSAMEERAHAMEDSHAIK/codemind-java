51377b62aa99b197049ae332d9e117389ebeb497
ADAMSAMEERAHAMEDSHAIK <142864954+ADAMSAMEERAHAMEDSHAIK@users.noreply.github.com>
2024-02-16 09:48:36 +0530

    import java.util.*;
    public class CodeMind{
        public static void main(String[] args){
            Scanner sc = new Scanner(System.in);
            String s = sc.nextLine();
            int c=0;
            for(int i=0;i<s.length();i++){
                if(s.charAt(i)==' '){
                    c++;
                }
            }
            System.out.println(c+1);
        }
    }

---
+++ Count_words_in_String.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class CodeMind{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        int c=0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)==' '){
+                c++;
+            }
+        }
+        System.out.println(c+1);
+    }
+}
