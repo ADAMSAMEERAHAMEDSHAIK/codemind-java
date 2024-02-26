0bbf4a414b2f867f0d5f4efa2b9de096c1b9fde1
ADAMSAMEERAHAMEDSHAIK <142864954+ADAMSAMEERAHAMEDSHAIK@users.noreply.github.com>
2024-02-26 11:17:54 +0530

    import java.util.*;
    public class CodeMind{
        public static void main(String args[]){
            Scanner sc = new Scanner(System.in);
            int n = sc.nextInt();
            if(n>=4 && n<100){
                int[] a = new int[n];
                for(int i=0;i<n;i++){
                    a[i] = sc.nextInt();
                }
                Set<Integer> F = new HashSet<>();
                for(int num : a){
                    F.add(num);
                }
                int[] b = new int[F.size()];
                int i=0; 
                for(int num : F){
                    b[i++] = num; 
                }
                Arrays.sort(b); 
                if(b.length>=3){
                    System.out.print(b[b.length - 3]); 
                }else{
                    System.out.print("It is not possible");
                }
            }else{
                System.out.print("It is not possible");
            }
        }
    }

--- Print_the_third_largest_number_in_the_array.java
+++ Print_the_third_largest_number_in_the_array.java
@@ -1,40 +1,31 @@
 import java.util.*;
 
 public class CodeMind{
-    public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        if(n<4 || n>=100){
-            System.out.println("It is not possible");
-            return;
-        }
-        int[] array = new int[n];
-        for(int i=0;i<n;i++){
-            array[i] = scanner.nextInt();
-        }
-        int thirdLargest = findThirdLargest(array);
-        
-        if(thirdLargest != Integer.MIN_VALUE){
-            System.out.println(thirdLargest);
-        }else{
-            System.out.println("It is not possible");
-        }
-    }
-    public static int findThirdLargest(int[] array){
-        int first, second, third;
-        first = second = third = Integer.MIN_VALUE;
-        for(int num : array){
-            if (num>first) {
-                third = second;
-                second = first;
-                first = num;
-            }else if(num>second && num<first) {
-                third = second;
-                second = num;
-            }else if(num>third && num<second){
-                third = num;
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n>=4 && n<100){
+            int[] a = new int[n];
+            for(int i=0;i<n;i++){
+                a[i] = sc.nextInt();
             }
+            Set<Integer> F = new HashSet<>();
+            for(int num : a){
+                F.add(num);
+            }
+            int[] b = new int[F.size()];
+            int i=0; 
+            for(int num : F){
+                b[i++] = num; 
+            }
+            Arrays.sort(b); 
+            if(b.length>=3){
+                System.out.print(b[b.length - 3]); 
+            }else{
+                System.out.print("It is not possible");
+            }
+        }else{
+            System.out.print("It is not possible");
         }
-        return third;
     }
 }
