f9e48ea2eac8654d4186b38074c669ed22d2842e
ADAMSAMEERAHAMEDSHAIK <142864954+ADAMSAMEERAHAMEDSHAIK@users.noreply.github.com>
2024-03-10 22:33:57 +0530

    import java.util.*;
    public class CodeMind{
        public static void main(String[] args){
            Scanner sc = new Scanner(System.in);
            int m = sc.nextInt();
            int n = sc.nextInt();
            if(m%2==0 || n%2==0){
                System.out.println("Player 1");
            }
            else{
                System.out.println("Player 2");
            }
        }
    }

---
+++ Predict_the_winner_in_Coin_Game.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class CodeMind{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        if(m%2==0 || n%2==0){
+            System.out.println("Player 1");
+        }
+        else{
+            System.out.println("Player 2");
+        }
+    }
+}
