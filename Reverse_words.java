6fd8880fef9d07d37f7ce2a91a69916e66f4e87d
ADAMSAMEERAHAMEDSHAIK <142864954+ADAMSAMEERAHAMEDSHAIK@users.noreply.github.com>
2024-03-11 10:07:32 +0530

    import java.util.*;
    public class ReverseWords {
        public static String reverseWords(String s){
            String[] words = s.trim().split("\s+");
            int start=0, end=words.length-1;
            while(start<end){
                String temp = words[start];
                words[start] = words[end];
                words[end] = temp;
                start++;
                end--;
            }
            return String.join(" ", words);
        }
    
        public static void main(String[] args){
            Scanner sc = new Scanner(System.in);
            String s1 = sc.nextLine();
            String c = reverseWords(s1);
            System.out.println(c);
        }
    }

---
+++ Reverse_words.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class ReverseWords {
+    public static String reverseWords(String s){
+        String[] words = s.trim().split("\s+");
+        int start=0, end=words.length-1;
+        while(start<end){
+            String temp = words[start];
+            words[start] = words[end];
+            words[end] = temp;
+            start++;
+            end--;
+        }
+        return String.join(" ", words);
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String s1 = sc.nextLine();
+        String c = reverseWords(s1);
+        System.out.println(c);
+    }
+}
